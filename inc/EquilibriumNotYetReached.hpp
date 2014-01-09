58e773978b75284c4d6de84fc9bfdd771cd414ab
Kazark <kazark@zoho.com>
2014-01-09 18:12:54 -0500

    Added equilibrium reached/not reached events

---
+++ inc/EquilibriumNotYetReached.hpp
@@ -0,0 +1,8 @@
+#ifndef _CONWAYGAMEOFLIFE_EQUILIBRIUMNOTYETREACHED_HPP_
+#define _CONWAYGAMEOFLIFE_EQUILIBRIUMNOTYETREACHED_HPP_
+namespace ConwayGameOfLife {
+	struct EquilibriumNotYetReached {
+        unsigned GenerationNumber;
+	};
+}
+#endif
---
+++ inc/EquilibriumReached.hpp
@@ -0,0 +1,8 @@
+#ifndef _CONWAYGAMEOFLIFE_EQUILIBRIUMREACHED_HPP_
+#define _CONWAYGAMEOFLIFE_EQUILIBRIUMREACHED_HPP_
+namespace ConwayGameOfLife {
+	struct EquilibriumReached {
+        unsigned GenerationNumber;
+	};
+}
+#endif
