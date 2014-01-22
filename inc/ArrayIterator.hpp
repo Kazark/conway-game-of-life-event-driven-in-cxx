ed91d2ccde14255e2d98dae709e6756efc8d1a36
Kazark <kazark@zoho.com>
2014-01-21 23:16:27 -0500

    Wumpused the beast!
    I beat the thing I have been trying to do for months. I built myself an
    easy way to iterate in C++. Note that ironically I namespaced it
    ::oog::core. That's because I intend this code to be part of the
    underlying C++ libraries for Oogaritic. It just happened to make sense
    to apply it to this project.

---
+++ inc/ArrayIterator.hpp
@@ -0,0 +1,43 @@
+#ifndef _OOG_CORE_ARRAYITERATOR_HPP_
+#define _OOG_CORE_ARRAYITERATOR_HPP_
+namespace oog {
+namespace core {
+    template<typename TElement, template<typename> class TArray>
+	class ArrayIterator {
+	public:
+        ArrayIterator(TArray<TElement> array) : array(array) {}
+
+        class const_iterator {
+        public:
+            const_iterator(const TArray<TElement>* arrayPtr, int index) :
+                arrayPtr(arrayPtr),
+                index(index)
+            {}
+            bool operator!=(const const_iterator& that) {
+                return this->index != that.index;
+            }
+            const_iterator operator++() {
+                auto i = *this;
+                index++;
+                return i;
+            }
+            TElement& operator*() {
+                return (*arrayPtr)[index];
+            }
+        private:
+            const TArray<TElement>* arrayPtr;
+            int index;
+        };
+
+        const_iterator begin() const {
+            return const_iterator(&array, 0);
+        }
+        const_iterator end() const {
+            return const_iterator(nullptr, array.length());
+        }
+	private:
+        TArray<TElement> array;
+	};
+}
+}
+#endif
---
+++ unittest/ArrayIterator.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+
+// Code under test
+#include "ArrayIterator.hpp"
+using namespace ::oog::core;
+
+#include <vector>
+
+template<typename TElement>
+class ArrayForTests {
+public:
+    ArrayForTests(TElement data[], int size) :
+        data(data),
+        size(size)
+    {}
+    TElement& operator[](int index) const {
+        return data[index];
+    }
+    int length() const {
+        return size;
+    };
+private:
+    TElement* data;
+    int size;
+};
+
+TEST(ArrayIteratorTests, can_iterate)
+{
+    std::vector<int> results;
+    int array[3] = {1, 3, 2};
+    ArrayIterator<int, ArrayForTests> iterator{ArrayForTests<int>{array, 3}};
+    for (auto element : iterator) {
+        results.push_back(element);
+    }
+    ASSERT_EQ(size_t(3), results.size());
+    EXPECT_EQ(1, results[0]);
+    EXPECT_EQ(3, results[1]);
+    EXPECT_EQ(2, results[2]);
+}
