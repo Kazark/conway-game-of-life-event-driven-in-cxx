99f9d5be398afb595487a57604945fd2dc2bc87e
Kazark <kazark@zoho.com>
2014-03-15 19:42:05 -0400

    Removed a couple of events
    I initially created these events will trying to build up some steam and
    gain some direction in the project. Now that I have build up some steam,
    I am not going in quite the same direction, so they are useless now.

--- inc/EquilibriumNotYetReached.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _CONWAYGAMEOFLIFE_EQUILIBRIUMNOTYETREACHED_HPP_
-#define _CONWAYGAMEOFLIFE_EQUILIBRIUMNOTYETREACHED_HPP_
-namespace ConwayGameOfLife {
-	struct EquilibriumNotYetReached {
-        unsigned GenerationNumber;
-	};
-}
-#endif
